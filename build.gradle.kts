c65ab184406ccbd87227edd803dd287e5a3a1524
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-04-06 13:22:21 -0700

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
 import net.minecrell.gitpatcher.PatchExtension
 
 plugins {
+  `maven-publish`
   id("net.minecraftforge.gitpatcher") version "0.10.+"
 }
+
+val ghRunNumber = System.getenv("GITHUB_RUN_NUMBER")?.let { "build.$it" } ?: "local"
+group = "io.papermc"
+version = "0.1+$ghRunNumber"
 
 configure<PatchExtension> {
   submodule = "work/Fernflower"
@@ -12,4 +17,24 @@ configure<PatchExtension> {
 
 tasks.register("rebuildPatches") {
   dependsOn(tasks.makePatches)
+}
+
+val ff = tasks.register("buildFernflower") {
+  doNotTrackState("Always run when requested")
+  outputs.file(layout.projectDirectory.file("fernflower.jar"))
+  doLast {
+    exec {
+      executable = layout.projectDirectory.file("build.sh").asFile.absolutePath
+    }
+  }
+}
+
+publishing {
+  repositories.maven("https://papermc.io/repo/repository/maven-releases/") {
+    name = "paper"
+    credentials(PasswordCredentials::class)
+  }
+  publications.create<MavenPublication>("maven") {
+    artifact(ff)
+  }
 }
